7a0150e9f5392d404ac9f9ffb2759741d6f1acf0
Evellin-Miyamoto <evellin.miyamoto@gmail.com>
2023-01-29 12:42:36 +0100

    emoji mouse
    Making the emoji follow the mouse

---
+++ mouse-position-emoji.js
@@ -0,0 +1,25 @@
+let x = 100;
+let y = 100;
+let s = 0.8;
+
+background(255, 255, 255);
+
+function emoji(x, y, s) {
+  //head
+  fill(255, 255, 0);
+  ellipse(x, y, 200 * s);
+
+  //eyes
+  fill(0, 0, 0);
+  ellipse(x - 40 * s, y - 30 * s, 20 * s, 20 * s);
+  ellipse(x + 40 * s, y - 30 * s, 20 * s, 20 * s);
+
+  //mouth
+  strokeWeight(2);
+  line(x - 30 * s, y + 30 * s, x + 30 * s, y + 30 * s);
+}
+
+function draw() {
+  clear();
+  emoji(mouseX, mouseY, 0.4);
+}
