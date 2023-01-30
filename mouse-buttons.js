7822471e82c74e16dac08cc32a99ef945b5b8e9f
Evellin-Miyamoto <evellin.miyamoto@gmail.com>
2023-01-30 14:20:00 +0100

    emoji on click
    draw emoji on click

--- mouse-buttons.js
+++ mouse-buttons.js
@@ -1,9 +1,28 @@
-let size = 20;
+// let size = 20;
 
-function draw() {
-  ellipse(width / 2, height / 2, size);
+// function draw() {
+//   ellipse(width / 2, height / 2, size);
+// }
+
+// function mouseClicked() {
+//   size = size + 10;
+// }
+
+function emoji(x, y, s) {
+  //head
+  fill(255, 255, 0);
+  ellipse(x, y, 200 * s);
+
+  //eyes
+  fill(0, 0, 0);
+  ellipse(x - 40 * s, y - 30 * s, 20 * s, 20 * s);
+  ellipse(x + 40 * s, y - 30 * s, 20 * s, 20 * s);
+
+  //mouth
+  strokeWeight(2);
+  line(x - 30 * s, y + 30 * s, x + 30 * s, y + 30 * s);
 }
 
 function mouseClicked() {
-  size = size + 10;
+  emoji(mouseX, mouseY, 0.5);
 }
